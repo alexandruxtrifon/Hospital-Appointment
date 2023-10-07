853750838e32560cb0423f3f06935da4fe804798
alextrifon <alex.trifon1337@gmail.com>
2023-10-07 15:48:20 +0300

    baza de date

---
+++ hospital-appointment-db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Pacient(nume VARCHAR(30) NOT NULL,
+					varsta SMALLINT(3) NOT NULL,
+					 telefon VARCHAR(15) NOT NULL,
+					 CodPacient SERIAL PRIMARY KEY
+					);
+CREATE TABLE Programare(prioritate)
