fe97da36dc798942c80125e047bf973c108d8fd8
alextrifon <alex.trifon1337@gmail.com>
2023-10-08 14:18:28 +0300

    baza de date gata i guess

--- hospital-appointment-db.sql
+++ hospital-appointment-db.sql
@@ -3,4 +3,11 @@ CREATE TABLE Pacient(nume VARCHAR(30) NOT NULL,
 					 telefon VARCHAR(15) NOT NULL,
 					 CodPacient SERIAL PRIMARY KEY
 					);
-CREATE TABLE Programare(prioritate)
+CREATE TABLE Programare(CodProgramare SERIAL PRIMARY KEY,
+						CodPacient INT REFERENCES Pacient(CodPacient),
+						Prioritate ENUM('Ridicata', 'Medie', 'Scazuta').
+						StatusProgramare ENUM('Programat', 'Preluat', 'Incheiata', 'Anulata')
+						);
+CREATE TABLE Config(CodConfig SERIAL PRIMARY KEY,
+				   DurataProgramare INT DEFAULT 30
+				   );						
